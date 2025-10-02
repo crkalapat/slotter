e5f242116abb733f7403146a1ab5d0f11d19d779
Charlie Kalapati <charlie.r.kalapati@gmail.com>
2025-10-01 22:02:26 -0500

    Add initial webpage files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main className="p-10">
+        <div className="flex justify-center">
+          <h1 className="text-3xl font-bold">
+            Slotter
+          </h1>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
