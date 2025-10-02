9fba3bffd6892223c06f3d745a445fc75b9d59d4
Charlie Kalapati <charlie.r.kalapati@gmail.com>
2025-10-02 17:01:24 -0500

    Create template small slot machine

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
-import { motion } from "motion/react"
-import './App.css'
+import { motion } from "motion/react";
+import "./App.css";
+import Slot from "./Slot.tsx";
 
 function App() {
-
   return (
     <>
       <main className="p-10">
         <div className="flex justify-center">
           <div className="flex flex-col">
-            <h1 className="text-3xl font-bold">
-              Slotter
-            </h1>
             <div className="flex justify-center">
-              <motion.button className="rounded-3xl bg-sky-700 p-3 w-20 hover:bg-sky-800 mt-10">
+              <motion.h1
+                className="text-5xl font-bold"
+                animate={{
+                  color: ["#FF0000", "#000000", "#440000", "#aa0000"],
+                }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  ease: "linear",
+                }}
+              >
+                Slotter
+              </motion.h1>
+            </div>
+            <div className="mt-20">
+              <div className="z-0 w-80 h-50 bg-red-500 rounded-lg flex flex-row items-center justify-evenly">
+                <Slot />
+                <Slot />
+                <Slot />
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <motion.button
+                className="rounded-3xl bg-sky-700 p-3 w-20 hover:bg-sky-800 mt-10 text-white"
+                whileHover={{ scale: 1.02 }}
+                whileTap={{ scale: 0.98 }}
+              >
                 Spin
               </motion.button>
             </div>
@@ -20,7 +43,7 @@ function App() {
         </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Slot.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function Slot() {
+  return (
+    <>
+      <div className="relative z-10 w-20 h-40 bg-yellow-50 rounded-sm text-6xl text-center flex justify-center items-center overflow-hidden">
+        🪙
+      </div>
+    </>
+  );
+}
+
+export default Slot;
