626f049bd75eb52be4f644349b4db8398682dec6
Charlie Kalapati <charlie.r.kalapati@gmail.com>
2025-10-02 21:24:22 -0500

    Add emojis and shadows to slot reels

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,31 @@ function App() {
         <div className="flex justify-center">
           <div className="flex flex-col">
             <div className="flex justify-center">
-              <motion.h1
-                className="text-5xl font-bold"
-                animate={{
-                  color: ["#FF0000", "#000000", "#440000", "#aa0000"],
-                }}
-                transition={{
-                  duration: 3,
-                  repeat: Infinity,
-                  ease: "linear",
-                }}
-              >
-                Slotter
-              </motion.h1>
+              <h1 className="text-5xl mt-10 font-bold">Slotter</h1>
             </div>
-            <div className="mt-20">
-              <div className="z-0 w-80 h-50 bg-red-500 rounded-lg flex flex-row items-center justify-evenly">
-                <Slot />
-                <Slot />
-                <Slot />
+            <div className="flex flex-row">
+              <div className="mt-20">
+                <div className="z-10 w-130 h-50 bg-red-500 rounded-2xl flex flex-row items-center justify-evenly float-left">
+                  <Slot />
+                  <Slot />
+                  <Slot />
+                  <Slot />
+                  <Slot />
+                </div>
+                {/* <div className="">
+                  <div
+                    dir="rtl"
+                    className="translate-x-130 translate-y-20 w-30 h-10 rounded-s-2xl bg-gray-200"
+                  ></div>
+                  <div className="translate-x-150 -translate-y-15 w-10 h-30 rounded-t-2xl bg-gray-200"></div>
+                  <div className="translate-x-150 -translate-y-50 w-10 h-10 rounded-t-2xl bg-red-500"></div>
+                </div> */}
               </div>
             </div>
+
             <div className="flex justify-center">
               <motion.button
-                className="rounded-3xl bg-sky-700 p-3 w-20 hover:bg-sky-800 mt-10 text-white"
+                className="rounded-full bg-red-800 p-3 w-20 hover:bg-red-900 mt-15 text-white"
                 whileHover={{ scale: 1.02 }}
                 whileTap={{ scale: 0.98 }}
               >
